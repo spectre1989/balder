b478867e676a291858c167755ef5b1aa84f01724
Joe Best-Rotheray <joebestrotheray@gmail.com>
2022-05-12 00:53:55 +0100

    initial commit

---
+++ Main.cpp
@@ -0,0 +1,13 @@
+#define VC_EXTRALEAN
+#define WIN32_LEAN_AND_MEAN
+#include <Windows.h>
+
+int WinMain(
+	_In_ HINSTANCE instance,
+	_In_opt_ HINSTANCE prev_instance,
+	_In_ LPSTR     cmd_line,
+	_In_ int       show_cmd
+)
+{
+	return 0;
+}
