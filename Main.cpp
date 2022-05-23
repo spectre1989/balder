4b6c23258ffc06d7f905a74340f62a4598b29bdb
Joe Best-Rotheray <joebestrotheray@gmail.com>
2022-05-23 21:58:18 +0100

    basic window and render loop

--- Main.cpp
+++ Main.cpp
@@ -1,13 +1,139 @@
 #define VC_EXTRALEAN
 #define WIN32_LEAN_AND_MEAN
 #include <Windows.h>
+#include <cstdint>
+
+
+typedef uint8_t uint8;
+
+
+static constexpr int c_frame_width = 640;
+static constexpr int c_frame_height = 480;
+static uint8 frame[c_frame_width * c_frame_height * 3];
+
+
+static constexpr int pixel(int x, int y)
+{
+	return ((y * c_frame_width) + x) * 3;
+}
+LRESULT wnd_proc(
+	HWND window,
+	UINT msg,
+	WPARAM wparam,
+	LPARAM lparam)
+{
+	switch (msg)
+	{
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		return 0;
+
+	default:
+		return DefWindowProcA(window, msg, wparam, lparam);
+	}
+}
 
 int WinMain(
 	_In_ HINSTANCE instance,
 	_In_opt_ HINSTANCE prev_instance,
 	_In_ LPSTR     cmd_line,
-	_In_ int       show_cmd
-)
+	_In_ int       show_cmd)
 {
-	return 0;
+	WNDCLASSA window_class = {};
+	window_class.lpfnWndProc = wnd_proc;
+	window_class.hInstance = instance;
+	window_class.lpszClassName = "balder";
+	RegisterClassA(&window_class);
+
+	// ps1 resolution was 640x480, that's the size of client rect we want, 
+	// adjust rect will calculate the window rect we need to have that size of 
+	// client rect
+	RECT window_rect = {};
+	window_rect.left = 100;
+	window_rect.right = window_rect.left + c_frame_width;
+	window_rect.top = 100;
+	window_rect.bottom = window_rect.top + c_frame_height;
+	AdjustWindowRect(&window_rect, WS_OVERLAPPEDWINDOW, false);
+
+	HWND window = CreateWindowExA(
+		0,
+		window_class.lpszClassName, 
+		"Balder", 
+		WS_OVERLAPPEDWINDOW, 
+		window_rect.left, window_rect.top, 
+		window_rect.right - window_rect.left, 
+		window_rect.bottom - window_rect.top,
+		0, 
+		0, 
+		instance, 
+		0);
+
+	ShowWindow(window, show_cmd);
+
+	LARGE_INTEGER clock_freq;
+	QueryPerformanceFrequency(&clock_freq);
+	LARGE_INTEGER last_frame_time;
+	QueryPerformanceCounter(&last_frame_time);
+	float frame_accumulator = 0.0f;
+	constexpr float c_framerate = 60.0f;
+	constexpr float c_frame_duration = 1.0f / c_framerate;
+	bool quit = false;
+	MSG msg;
+	int a = 0;
+	while (!quit)
+	{
+		while (PeekMessageA(&msg, window, 0, 0, PM_REMOVE))
+		{
+			TranslateMessage(&msg);
+			DispatchMessageA(&msg);
+
+			if (msg.message == WM_QUIT)
+			{
+				quit = true;
+				break;
+			}
+		}
+
+		LARGE_INTEGER now;
+		QueryPerformanceCounter(&now);
+		const LONGLONG dt = (now.QuadPart - last_frame_time.QuadPart);
+		const float dt_s = dt / (float)clock_freq.QuadPart;
+		frame_accumulator += dt_s;
+		last_frame_time = now;
+
+		while (frame_accumulator >= c_frame_duration)
+		{
+			frame_accumulator -= c_frame_duration;
+
+			HDC dc = GetDC(window);
+
+			BITMAPINFO bitmap_info = {};
+			bitmap_info.bmiHeader.biSize = sizeof(bitmap_info.bmiHeader);
+			bitmap_info.bmiHeader.biWidth = c_frame_width;
+			bitmap_info.bmiHeader.biHeight = c_frame_height;
+			bitmap_info.bmiHeader.biPlanes = 1;
+			bitmap_info.bmiHeader.biBitCount = 24;
+			bitmap_info.bmiHeader.biCompression = BI_RGB;
+
+			memset(frame, 0, sizeof(frame));
+
+			SetDIBitsToDevice(
+				dc,
+				0, 0,
+				c_frame_width, c_frame_height,
+				0, 0,
+				0,
+				c_frame_height,
+				frame,
+				&bitmap_info,
+				DIB_RGB_COLORS
+			);
+
+			ReleaseDC(window, dc);
+		}
+
+		// TODO sleep unused cycles
+	}
+
+	return msg.wParam;
 }
